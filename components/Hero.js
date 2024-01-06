61096010a7e2f42acfc68aeca1b612edabd00c24
mitesh-24 <miteshhk05@gmail.com>
2024-01-06 13:00:35 +0530

    typewriter intro added

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,57 @@
+"use client"
+import { useEffect, useState } from 'react';
+
 const Hero = () => {
-    return(
-        <div>
-            Hello I am Hero
-        </div>
-    )
-}
+  const words = ["Hello, World!!!", "I am Mitesh Kulkarni!!", "Welcome to my website!!", "This is my linux themed Portfolio  "];
+  let i = 0;
+  let j = 0;
+  let currentWord = "";
+  let isDeleting = false;
+  const [displayText, setDisplayText] = useState("_");
+
+  useEffect(() => {
+    function type() {
+        currentWord = words[i];
+      
+        setDisplayText((prevText) => {
+          let newText;
+      
+          if (isDeleting) {
+            newText = currentWord.substring(0, j) + "_";
+            j--;
+      
+            if (j < 0) {
+              isDeleting = false;
+              i++;
+              if (i === words.length) {
+                i = 0;
+              }
+            }
+          } else {
+            newText = currentWord.substring(0, j + 1) + "_";
+            j++;
+      
+            if (j > currentWord.length) {
+              isDeleting = true;
+            }
+          }
+      
+          return newText;
+        });
+      
+        setTimeout(type, 200);
+      }
+
+    type();
+  }, []);
+
+  return (
+    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto mt-20">
+        <h1 id="typewriter" className="text-4xl font-bold text-white">
+          {displayText}
+        </h1>
+    </div>
+  );
+};
 
 export default Hero;
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,12 @@
 'use client'
+import Link from "next/link"
 
 const NavBar = () => {
 
     return(
         <nav className="dark:bg-black">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://github.com/mitesh-24" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="https://github.com/mitesh-24" target="_blank" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://www.debian.org/Pics/debian-logo-1024x576.png" className="h-8" alt="logo" />
                     <span className="sel-center text-2xl font-semibold whitespace-nowrap text-green-500 dark:text-green-300">
                         Portfolio
@@ -15,19 +16,16 @@ const NavBar = () => {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-600 rounded-lg bg-gray-900 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
                         <li>
-                        <a href="#" className="block py-2 px-3 text-green-500 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
+                        <Link href="#" className="block py-2 px-3 text-green-500 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</Link>
                         </li>
                         <li>
-                        <a href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                        <Link href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Me</Link>
                         </li>
                         <li>
-                        <a href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                        <Link href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Resume</Link>
                         </li>
                         <li>
-                        <a href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                        </li>
-                        <li>
-                        <a href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        <Link href="#" className="block py-2 px-3 text-gray-400 rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-green-300 md:p-0 dark:text-green-300 md:dark:hover:text-green-300 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
                         </li>
                     </ul>
                 </div>
